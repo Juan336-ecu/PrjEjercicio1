b1f2266542082b76c438bfefd6d7617eceeffa16
juanc <juanc@DESKTOP-CBSBR59>
2020-05-20 18:15:24 -0500

    Version 1

---
+++ PryEjercicio-1/src/test/java/com/velasco/ejercicio/PryEjercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velasco.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
